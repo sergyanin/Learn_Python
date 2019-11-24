925c75608ec008919397f120d4686566e6ce3d9b
sergyanin <cryptonnov@gmail.com>
2019-11-24 23:08:23 +0300

    Решил вопрос с запросом и выводом по городу

--- mod_re.py
+++ mod_re.py
@@ -8,24 +8,20 @@ w = 'Иркутск, доброе утро! Для вас тут нашлись 
 
 t_list = re.findall(r'[А-ЯЁ]\w+', w)
 t_list = list(set(t_list) - set(prepositions))
-print(t_list)
+# print(t_list)
 
 db = sqlite3.connect('countries.sqlite')
 c = db.cursor()
-rg = 'Иркутск'
+rg = 'Пекин'
 
-c.execute("SELECT country_id FROM city WHERE name= ?", (rg,))
-res = c.fetchall()
+# c.execute("SELECT country_id FROM city WHERE name= ?", (rg,))
+# res = c.fetchall()
+# c.execute("SELECT name FROM country WHERE country_id= ?", (res[0][0],))
+# res_country = c.fetchall()
+# print(res_country[0][0])
 
-print(res)
-
-
-
-
-
-
-
-
-
+c.execute("SELECT city.name, country.name  FROM country LEFT JOIN city ON country.country_id = city.country_id WHERE city.name = ?", (rg,))
+res_country_1 = c.fetchall()
+print(res_country_1)
 db.commit()
 db.close()
