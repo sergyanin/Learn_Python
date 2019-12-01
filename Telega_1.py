4fb5ee4afe24ec00da8d9947d0695af44a5e48ca
sergyanin <cryptonnov@gmail.com>
2019-12-01 15:47:43 +0300

    решил вопрос со временем

--- Telega_1.py
+++ Telega_1.py
@@ -1,6 +1,9 @@
 from telethon import TelegramClient, sync, events
 import re
 import sqlite3
+import time
+from datetime import datetime
+from datetime import timedelta
 
 
 # Вставляем api_id и api_hash
@@ -15,9 +18,11 @@ client = TelegramClient('Second', api_id, api_hash)
 @client.on(events.NewMessage(chats=(-1001164207750)))
 async def normal_handler(event):
     # print(event.message)
-    mess_dat = event.message.to_dict()['date'].strftime("%d.%m.%Y\n%H:%M\n")
+    # event.message.to_dict()['date'].strftime("%d.%m.%Y\n%H:%M\n")
+    mess_dat = (datetime.utcnow() + timedelta(hours=3)
+                ).strftime("%d.%m.%Y\n%H:%M\n")
     user_mess = event.message.to_dict()['message']
-    full_msg = '**'+ mess_dat + '**' + '\n'+ user_mess
+    full_msg = f'**{mess_dat}**\n{user_mess}'
     # print(mess_date = str(event.message.to_dict()['date']))
     await client.send_message(-1001329672585, full_msg, parse_mode='md')
 
@@ -25,9 +30,10 @@ async def normal_handler(event):
 @client.on(events.NewMessage(chats=(-1001141664489)))
 async def normal_handler(event):
     # print(event.message)
-    mess_dat = event.message.to_dict()['date'].strftime("%d.%m.%Y\n%H:%M\n")
+    mess_dat = (datetime.utcnow() + timedelta(hours=3)
+                ).strftime("%d.%m.%Y\n%H:%M\n")    
     user_mess = event.message.to_dict()['message']
-    full_msg = '**' + mess_dat + '**' + '\n' + user_mess
+    full_msg = f'**{mess_dat}**\n{user_mess}'
     # print(mess_date = str(event.message.to_dict()['date']))
     await client.send_message(-1001329672585, full_msg, parse_mode='md')
 
@@ -35,13 +41,19 @@ async def normal_handler(event):
 @client.on(events.NewMessage(chats=(-1001067688841)))
 async def normal_handler(event):
     # print(event.message)
-    mess_dat = event.message.to_dict()['date'].strftime("%d.%m.%Y\n%H:%M\n")
+    mess_dat = (datetime.utcnow() + timedelta(hours=3)
+                ).strftime("%d.%m.%Y\n%H:%M\n")    
     user_mess = event.message.to_dict()['message']
-    full_msg = '**' + mess_dat + '**' + '\n' + user_mess
+    full_msg = f'**{mess_dat}**\n{user_mess}'
     # print(mess_date = str(event.message.to_dict()['date']))
     await client.send_message(-1001329672585, full_msg, parse_mode='md')
+while True:
+    try:
+        client.start()
+        
+    except Exception as e:
 
-client.start()
-# client.send_message(-1001329672585,
-#                     (normal_handler.mess_date + '\n' + normal_handler.user_mess))
-client.run_until_disconnected()
+        print(e)
+        time.sleep(15)
+        client.run_until_disconnected()
+    
--- mod_re.py
+++ mod_re.py
@@ -1,27 +1,79 @@
 import re
 import sqlite3
+from sqlite3 import Error
+
+
+# def sql_connection():
+
+#     try:
+#         con = sqlite3.connect('countries.sqlite')
+#         print("Connection is established: Database is created in memory")
+#     except Error:
+#         print(Error)
+#     finally:
+#         con.close()
+
+
+# sql_connection()
+
+# Выдача из трёх таблиц одновременно c использованием функции
+
+def sql_select(word):
+    return f"SELECT city.name, region.name, country.name  FROM country LEFT JOIN region ON country.country_id = region.country_id LEFT JOIN city ON region.region_id = city.region_id WHERE city.name = '{word}'"
+
+
+def sql_select_country(word):
+    return f"SELECT city.name, country.name  FROM country LEFT JOIN city ON country.country_id = city.country_id WHERE city.name = '{word}'"
+
+
+def sql_insert(word):
+    pass
+
+
+def sql_search(word):
+    db = sqlite3.connect('countries.sqlite')
+    c = db.cursor()
+    # c.execute(sql_select(word))
+    c.execute(sql_select_country(word))
+    found_item = c.fetchall()
+    db.commit()
+    db.close()
+    return found_item
+
 
 prepositions = ['В', 'Без', 'До', 'Из', 'К', 'На', 'По', 'О', 'От', 'Перед',
-                'При', 'Через', 'С', 'У', 'И', 'Нет', 'За', 'Над', 'Для', 'Об', 'Под', 'Про']
+                'При', 'Через', 'С', 'У', 'И', 'Нет', 'За', 'Над', 'Для', 'Об', 'Под', 'Про', 'Тур',]
 
-w = 'Иркутск, доброе утро! Для вас тут нашлись недорогие билетики на Бали - 27500 рублей за полет туда-обратно! По пути - безвизовые Пекин и Куала-Лумпур.'
+w = 'Питер! Своё родное: Тур в Сочи на неделю за 4100 с каждого. Старт 30 ноября из Пулково: '
 
 t_list = re.findall(r'[А-ЯЁ]\w+', w)
-t_list = list(set(t_list) - set(prepositions))
-# print(t_list)
+cities = list(set(t_list) - set(prepositions))
+# print(cities)
 
-db = sqlite3.connect('countries.sqlite')
-c = db.cursor()
-rg = 'Дзержинск'
-
+# db = sqlite3.connect('countries.sqlite')
+# c = db.cursor()
+# print(sql_sel(rg))
 # c.execute("SELECT country_id FROM city WHERE name= ?", (rg,))
 # res = c.fetchall()
 # c.execute("SELECT name FROM country WHERE country_id= ?", (res[0][0],))
 # res_country = c.fetchall()
 # print(res_country[0][0])
 
-c.execute("SELECT city.name, region.name, country.name  FROM country LEFT JOIN region ON country.country_id = region.country_id LEFT JOIN city ON region.region_id = city.region_id WHERE city.name = ?", (rg,))
-res_country_1 = c.fetchall()
-print(res_country_1)
-db.commit()
-db.close()
+
+# c.execute("SELECT city.name FROM country LEFT JOIN region ON country.country_id = region.country_id LEFT JOIN city ON region.region_id = city.region_id WHERE city.name = ?", (rg,))
+# res_country_1 = c.fetchall()
+# print(res_country_1)
+found_list = []
+for city in cities:
+    try:
+        if sql_search(city):
+            found_list.append(sql_search(city))
+
+    except Error:
+        print(Error)
+    
+print(found_list)
+
+
+# db.commit()
+# db.close()
